b9c24790f51d5cd8bcab53576fd662e27d52f54b
jc-fun <urainstar@gmail.com>
2023-02-28 20:33:03 +0800

    daily update

---
+++ Project/web-demo/ws/webservice/src/bin/teacher-service.rs
@@ -0,0 +1,53 @@
+/*
+ * @Author: jc-fun urainstar@gmail.com
+ * @Date: 2023-02-28 12:56:05
+ * @LastEditors: jc-fun urainstar@gmail.com
+ * @LastEditTime: 2023-02-28 19:38:50
+ * @FilePath: /ws/webservice/src/bin/teacher-service.rs
+ * @Description:
+ */
+use actix_web::{web, App, HttpServer};
+use dotenv::dotenv;
+use sqlx::postgres::PgPoolOptions;
+use std::env;
+use std::io;
+use std::sync::Mutex;
+
+#[path = "../handlers.rs"]
+mod handlers;
+
+#[path = "../routers.rs"]
+mod routers;
+
+#[path = "../state.rs"]
+mod state;
+
+#[path = "../models.rs"]
+mod models;
+
+use routers::*;
+use state::AppState;
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not found");
+    let db_pool = PgPoolOptions::new().connect(&database_url).await.unwrap();
+
+    let shared_data = web::Data::new(AppState {
+        health_check_response: "I' m OK.".to_string(),
+        visit_count: Mutex::new(0),
+        // courses: Mutex::new(vec![]),
+        db: db_pool,
+    });
+
+    let app = move || {
+        App::new()
+            .app_data(shared_data.clone())
+            .configure(general_routes)
+            .configure(course_routes)
+    };
+
+    HttpServer::new(app).bind("127.0.0.1:3000")?.run().await
+}
---
+++ Project/web-demo/ws/webservice/src/routers.rs
@@ -0,0 +1,23 @@
+/*
+ * @Author: jc-fun urainstar@gmail.com
+ * @Date: 2023-02-28 12:46:54
+ * @LastEditors: jc-fun urainstar@gmail.com
+ * @LastEditTime: 2023-02-28 17:07:13
+ * @FilePath: /ws/webservice/src/routers.rs
+ * @Description:
+ */
+use super::handlers::*;
+use actix_web::web;
+
+pub fn general_routes(cfg: &mut web::ServiceConfig) {
+    cfg.route("/health", web::get().to(health_check_handler));
+}
+
+pub fn course_routes(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::scope("/courses")
+            .route("/", web::post().to(new_course))
+            .route("/{user_id}", web::get().to(get_courses_for_teacher))
+            .route("/{user_id}/{course_id}", web::get().to(get_course_detail))
+    );
+}
